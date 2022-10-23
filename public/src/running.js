6452229c6d63f82f6ae22ff62fdb2cc7864b6b8a
bulgat <sirbulgat@gmail.com>
2022-10-23 23:33:47 +0300

    add dead screen

--- public/src/running.js
+++ public/src/running.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector(".hero");
 const heroBoy = document.querySelector(".heroBoy");
-const vilan = document.querySelector(".vilan");
+const ghost = document.querySelector(".vilan");
 const pursuer = document.querySelector(".pursuer");
 const heart = document.querySelector(".heart");
 const heart0 = document.querySelector(".heart0");
@@ -10,17 +10,18 @@ const scoreText = document.querySelector(".scoreText");
 const timeText = document.querySelector(".timeText");
 const container = document.querySelector(".container");
 
-let _vilanPosition = {x:450,testJump:false};
-let _deadHero = false;
+let _ghostPosition = {x:450,testJump:false};
+let _deadHero = {dead:false,time:0,countLife:3,score:0};
+
 let _moveLeft = 80;
 let _heroBoyPosition = 100;
 let _moveRight = 0;
-let _score = 0;
+//let _score = 0;
 let _time=0;
 
 let heroJump = false;
 let _heroJumpTime = 0;
-let countLife = 3;
+//let countLife = 3;
 
 tomb.style.visibility = 'hidden';
 
@@ -37,8 +38,24 @@ function jump() {
 		//container.style.backgroundImage = "url('public/fortestJungle.gif')";
 		//container.style.backgroundImage.background-size = "100%";
 	}, 500);
+	//ressurection
+	if (_deadHero.dead===true) {
+		container.style.backgroundImage = "url('public/forest1.gif')";
+		ghost.style.visibility = "visible";
+		pursuer.style.visibility = "visible";
+		_moveRight =0;
+		pursuer.style.left =_moveRight +'px';
+		tomb.style.visibility = "hidden";
+		hero.style.visibility = 'visible';
+		heart.style.visibility = 'visible';
+		heart0.style.visibility = 'visible';
+		heart1.style.visibility = 'visible';
 
 
+		_deadHero.dead=false;
+		_deadHero.countLife =3;
+		_deadHero.score =0;
+	}
 }
 
 
@@ -51,55 +68,57 @@ document.addEventListener("mousedown",function(event) {
 
 let isAlive =setInterval(function() {
 	let heroTop = parseInt(window.getComputedStyle(hero).getPropertyValue("top"));
-	let vilanLeft = parseInt(window.getComputedStyle(vilan).getPropertyValue("top"));
+	let ghostLeft = parseInt(window.getComputedStyle(ghost).getPropertyValue("top"));
 
-	if(vilanLeft<40 && vilanLeft>20 && heroTop <=130) {
-		vilan.style.animation = "none";
+	if(ghostLeft<40 && ghostLeft>20 && heroTop <=130) {
+		ghost.style.animation = "none";
 		
 	}
 
-	_vilanPosition.x-=1;
+	_ghostPosition.x-=1;
 
-	vilan.style.left = _vilanPosition.x +'px';
+	ghost.style.left = _ghostPosition.x +'px';
 
-	if(_vilanPosition.x<0){
-		_vilanPosition.x = 450;
-		_vilanPosition.testJump = false;
-		_score++;
+	if(_ghostPosition.x<0){
+		_ghostPosition.x = 450;
+		_ghostPosition.testJump = false;
+		_deadHero.score++;
 	}
-	if(_vilanPosition.testJump === false){
-		if(_vilanPosition.x>_heroBoyPosition+10 &&  _vilanPosition.x < _heroBoyPosition+20)
+	if(_ghostPosition.testJump === false){
+		if(_ghostPosition.x>_heroBoyPosition+10 &&  _ghostPosition.x < _heroBoyPosition+20)
 		{
-			_vilanPosition.testJump = true;
+			_ghostPosition.testJump = true;
 			
 			if (heroJump==false) {
 				//damage  hero
 				//reset hero
 				_heroBoyPosition = 100;
 
-				if (countLife==3){
+				if (_deadHero.countLife==3){
 					heart.style.visibility = 'hidden';
-					countLife--;
+					_deadHero.countLife--;
 					return;
 				}
-				if (countLife==2){
+				if (_deadHero.countLife==2){
 					heart0.style.visibility = 'hidden';
-					countLife--;
+					_deadHero.countLife--;
 					return;
 				}
-				if (countLife==1){
+				if (_deadHero.countLife==1){
 					heart1.style.visibility = 'hidden';
-					countLife--;
+					_deadHero.countLife--;
 					return;
 				}
-				if (countLife==0){
-					deadHero();
+				if (_deadHero.countLife==0){
+					if (_deadHero.dead===false) {
+						deadHero();
+					}
 				}
 			}
 
 		}
 	}
-	if (_deadHero===true) {
+	if (_deadHero.dead===true) {
 		if(_moveLeft>=0){
 			hero.style.left = _moveLeft+'px';
 			tomb.style.left = _moveLeft+'px';
@@ -122,7 +141,8 @@ let isAlive =setInterval(function() {
 			let jumpHeight = _time-_heroJumpTime;
 			hero.style.top = (150-jumpHeight*7)+'px';
 			
-			//console.log( (150-jumpHeight*7)+" ## heroJump = "+jumpHeight)
+			
+			
 		} 
 		
 		if(_time>=_heroJumpTime+coefJumpUp){
@@ -134,9 +154,21 @@ let isAlive =setInterval(function() {
 		}
 		
 	}
-
-
-	scoreText.textContent ="      Score:"+_score;
+   //dead заставка
+   if (_deadHero.dead===true) {
+		console.log((_deadHero.time+500)+  " # um  = "+_deadHero.time +"   t = "+_time);
+		if(_deadHero.time+500<_time)
+		{
+			console.log("  dead screen");
+			container.style.backgroundImage = "url('public/forestDead.gif')";
+			ghost.style.visibility = "hidden";
+			pursuer.style.visibility = "hidden";
+			tomb.style.visibility = "hidden";
+		}
+   } else {
+		scoreText.textContent ="      Score:"+_deadHero.score;
+   }
+	
 	timeText.textContent ="      Time:"+_time;
 	_time++;
 	
@@ -144,5 +176,6 @@ let isAlive =setInterval(function() {
 function deadHero() {
 	tomb.style.visibility = 'visible';
 	hero.style.visibility = 'hidden';
-	_deadHero = true;
+	_deadHero.dead = true;
+	_deadHero.time = _time;
 }
